b2fbc09ebffa6dc5b8a9a9e7f74a8b683a0b9359
Elisa Monacchi <elisa.monacchi@gmail.com>
2020-01-26 18:17:10 +0800

    404 page (#8)
    * Added 404 page
    
    * added 404 page in the content file
    
    * finalized the 404

--- src/content.js
+++ src/content.js
@@ -12,5 +12,12 @@ module.exports = [
       title: "tweak - browser extension - documentation"
     },
     template: "./src/pages/documentation.hbs"
+  },
+  {
+    output: "./404.html",
+    content: {
+      title: "tweak - 404 page not found"
+    },
+    template: "./src/pages/404.hbs"
   }
 ];
