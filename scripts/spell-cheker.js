c082390ac3852b28840bc06b818f005693374f7c
Daniel Caldas <11733994+danielcaldas@users.noreply.github.com>
2020-02-02 12:44:57 +0800

    Update documentation and other misc improvements (#19)
    * Remove spell-checker script and removed spellcheker dependency
    
    * Rename scripts folder to tools
    
    * Update FAQs
    
    * Add CNAME file

--- scripts/spell-cheker.js
+++
@@ -1,9 +0,0 @@
-const spellChecker = require('spellchecker');
-const htmlToText = require('html-to-text');
-
-console.log(spellChecker.checkSpelling('this is bd written'));
-// [ { start: 5, end: 13 } ]
-const input = '<h1>bad writting thgns english is nott a godd langusadfasfage</h1>';
-const res = spellChecker.checkSpelling(htmlToText.fromString(input));
-
-res.forEach(r => console.log(`Typo detected: "(...) ${input.slice(r.start, r.end)} (...)" at ${r.start}:${r.end}`));
